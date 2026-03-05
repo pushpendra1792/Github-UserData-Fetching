1885312734d5bb4b3d59543c9c27e1d3d68303b1
Raghvendra2405 <raghvendrasingh2405@gmail.com>
2026-03-05 10:19:36 +0530

    Refactor form submission handling and update user info fetching logic

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-let myform = document.getElementById('myform');
-myform.addEventListener('submit',function(event){
-    event.preventDefault();
-});
 
-let username = document.getElementById('username').value;
-console.log(username);
+        let myform = document.getElementById('myform');
 
-function getUserInfo(){
-    return fetch(`https://api.github.com/users/pushpendra1792`).then((raw)=> raw.json())
-}
+        myform.addEventListener('submit', function (event) {
+            event.preventDefault();
 
-getUserInfo().then(function(data){
-    console.log(data);
-})
+            let username = document.getElementById('username').value;
+            console.log(username);
+
+            getUserInfo(username).then(function (data) {
+                console.log(data);
+            });
+        });
+
+        function getUserInfo(username) {
+            return fetch(`https://api.github.com/users/${username}`)
+                .then((raw) => raw.json());
+        }
+
