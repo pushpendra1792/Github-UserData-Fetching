f1c3872731156355af6f8e007eae0c64759a91d8
Pushpendra Pal Singh Rathore <luckyrathore1792@gmail.com>
2026-03-05 11:37:54 +0530

    Fixed Dynamic logic of Fetching UserData

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
+let myform = document.getElementById('myform');
+let basic_info = document.querySelector('.basic_info');
+let image = document.querySelector('.image');
 
-        let myform = document.getElementById('myform');
+myform.addEventListener('submit', function (event) {
+    event.preventDefault();
 
-        myform.addEventListener('submit', function (event) {
-            event.preventDefault();
+    let username = document.getElementById('username').value;
+    console.log(username);
 
-            let username = document.getElementById('username').value;
-            console.log(username);
+    getUserInfo(username).then(function (data) {
+        console.log(data);
+        let name = data.name;
+        let followers = data.followers;
+        let following = data.following;
+        let public_repos = data.public_repos;
+        let image_url = data.avatar_url;
 
-            getUserInfo(username).then(function (data) {
-                console.log(data);
-            });
-        });
+        let info = `
+                <h2>Name = ${name}</h2>
+                <h2>Username = ${username}</h2>
+                <h2>Followers = ${followers}</h2>
+                <h2>Following = ${following}</h2>
+                <h2>Public Repos = ${public_repos}</h2>`
 
-        function getUserInfo(username) {
-            return fetch(`https://api.github.com/users/${username}`)
-                .then((raw) => raw.json());
-        }
+        let img_info = `<img id="img" src="${image_url}" alt="profile">`
+        basic_info.innerHTML = info;
+        image.innerHTML = img_info;
+    });
+});
 
+function getUserInfo(username) {
+    return fetch(`https://api.github.com/users/${username}`)
+    .then((raw) => raw.json());
+}
